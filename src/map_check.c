05583177a5e4d6dd9006f1cb6475d986dbca955d
arthurpnl <arthurpnlcontact1@gmail.com>
2025-04-08 19:13:54 +0200

    working malloc **

--- src/map_check.c
+++ src/map_check.c
@@ -13,7 +13,7 @@
 #include "../includes/so_long.h"
 #include <unistd.h>
 
-int	*count_line(char *file)
+int	count_line(char *file)
 {
 	int	fd;
 	int	count;
@@ -32,30 +32,43 @@ int	*count_line(char *file)
 	return (count);
 }
 
-char	**parse_map(char *file)
+char **parse_map(char *file)
 {
-	int	fd;
-	int	i;
-	char	**map;
-	char	*line;
+	int fd;
+	int i;
+	char **map;
+	char *line;
 
 	i = 0;
-	fd = open(file, O_RDONLY);
-	map = malloc(sizeof(char *) * (count_line(file) + 1);
-	if (fd == -1)
-		return (0);
+	int line_count = count_line(file);
 
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+
+	map = malloc(sizeof(char *) * (line_count + 1));
 	if (!map)
 	{
 		close(fd);
 		return (NULL);
 	}
+
+	close(fd);
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		free(map);
+		return (NULL);
+	}
+
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		map[i] = line;
 		i++;
 	}
 	map[i] = NULL;
+
+
 	close(fd);
 	return (map);
 }
