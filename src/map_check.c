4759d0326330ad05bdd9f6d290a2543618974dd0
arthurpnl <arthurpnlcontact1@gmail.com>
2025-04-08 17:31:12 +0200

    parse map add

---
+++ src/map_check.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_check.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 16:08:13 by arpenel           #+#    #+#             */
+/*   Updated: 2025/04/08 16:08:44 by arpenel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+#include <unistd.h>
+
+int	*count_line(char *file)
+{
+	int	fd;
+	int	count;
+	char	*line;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	count = 0;
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		count++;
+		free(line);
+	}
+	return (count);
+}
+
+char	**parse_map(char *file)
+{
+	int	fd;
+	int	i;
+	char	**map;
+	char	*line;
+
+	i = 0;
+	fd = open(file, O_RDONLY);
+	map = malloc(sizeof(char *) * (count_line(file) + 1);
+	if (fd == -1)
+		return (0);
+
+	if (!map)
+	{
+		close(fd);
+		return (NULL);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		map[i] = line;
+		i++;
+	}
+	map[i] = NULL;
+	close(fd);
+	return (map);
+}
+
+
