cd22a89d838defdf389da85be3a8e73f22b723d4
arthurpnl <arthurpnlcontact1@gmail.com>
2025-04-09 19:08:21 +0200

    add parse.c for map file validation and parsing logic

---
+++ src/parse.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 16:56:50 by arpenel           #+#    #+#             */
+/*   Updated: 2025/04/09 16:56:53 by arpenel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+int	parser(t_data	*game, char	**av)
+{
+	if (is_ber_file(*av) == FAILURE)
+		return (FAILURE);
+	if (file_exists(*av) == FAILURE)
+		return (FAILURE);
+	if (parse_map(*av, game) == FAILURE)
+		return (FAILURE);
+	return (SUCCESS);
+}
