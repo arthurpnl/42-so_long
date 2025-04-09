f9de97c110953758e40aa3b918b9e33c13972dab
arthurpnl <arthurpnlcontact1@gmail.com>
2025-04-09 19:13:14 +0200

    refactor main.c to streamline argument handling and integrate game data structure

--- src/main.c
+++ src/main.c
@@ -14,57 +14,13 @@
 
 #include "so_long.h"
 
-int main(int argc, char **argv)
-{
-	char **map;
-
-	if (argc != 2)
-	{
-		write(2, "Usage: ./so_long <map.ber>\n", 27);
-		return (1);
-	}
-
-	if (!is_ber_file(argv[1]))
-		return (1);
-
-	if (!file_exists(argv[1]))
-		return (1);
-
-	map = parse_map(argv[1]);
-	if (!map)
-	{
-		write(2, "Error parsing map\n", 18);
-		return (1);
-	}
-
-	// Debug : afficher la map
-	int i = 0;
-	while (map[i] != NULL)
-	{
-		printf("Ligne %d: %s", i, map[i]);
-		i++;
-	}
-
-	// N'oubliez pas de libérer la mémoire
-	i = 0;
-	while (map[i] != NULL)
-	{
-		free(map[i]);
-		i++;
-	}
-	free(map);
-
-	return (0);
-}
-
-/*
 int	main(int	argc, char **argv)
 {
+	t_data	*game;
 	if (argc != 2)
 		return (0);
 	else
 		is_ber_file(argv[1]);
-	parse_map(char *file)
+	parse_map(*argv[1], *game);
 	return (0);
 }
- */
