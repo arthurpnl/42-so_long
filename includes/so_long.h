2ed2dd5d43ce1dd06501b5955280fd527f5345d7
arthurpnl <arthurpnlcontact1@gmail.com>
2025-04-09 19:09:17 +0200

    update so_long.h to define SUCCESS and FAILURE constants, add file and map check function prototypes, and enhance t_data structure

--- includes/so_long.h
+++ includes/so_long.h
@@ -1,7 +1,30 @@
-#ifndef SO_LONG
-# define SO_LONG
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-#include <stdio.h>
+# include "../Libft/libft.h"
+#include <unistd.h>
+# define SUCCESS 1
+# define FAILURE 0
 
+// file check //
+int	is_ber_file(char	*file);
+int	file_exists(char	*file);
+
+// map check //
+bool    parse_map(char *file, t_data	*game);
+int	count_line(char *file, t_data *game);
+
+typedef struct s_data
+{
+    char    **map;
+    int		line_count;
+    int     len_line;
+    int     size_map;
+    int     player_x;
+    int     player_y;
+    int     exit_count;
+    int     collectible_count;
+    int     player_count;
+} t_data;
 
 #endif
