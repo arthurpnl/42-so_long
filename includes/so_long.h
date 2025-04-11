3de69dbe383464773e9d7149b7464d8d7aa281c8
arthurpnl <arthurpnlcontact1@gmail.com>
2025-04-11 17:32:39 +0200

    Refactor data structures and add path-check initialization.
    Renamed `s_data` to `s_data_map` and introduced a new `s_path_check` structure for managing path validation. Added `start_x` and `start_y` fields to track the starting position. Extended initialization functions to include path-check setup.

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,12 +6,14 @@
 # define SUCCESS 1
 # define FAILURE 0
 
-typedef struct s_data
+typedef struct s_data_map
 {
     char    **map;
     int		line_count;
     int     len_line;
     int     size_map;
+    int	    start_x;
+    int     start_y;
     int     player_x;
     int     player_y;
     int     exit_count;
@@ -19,6 +21,14 @@ typedef struct s_data
     int     player_count;
 } t_data;
 
+typedef struct s_path_check
+{
+    char    **visited;
+    int		collectible_found;
+    bool	exit_found;
+    int     i;
+    int     j;
+} t_path_check;
 
 // file check //
 bool	is_ber_file(char	*file);
@@ -36,8 +46,8 @@ bool	check_collectible(t_data *game);
 bool	check_exit(t_data *game);
 bool	check_start_position(t_data *game);
 
-
 // init game //
 void	init_game(t_data *game);
+void	init_game_path(t_path_check *path);
 
 #endif
